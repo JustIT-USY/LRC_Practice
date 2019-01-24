91d8f4de7abf3feee4fc3d93ad2df6beda79c52c
Xlcjs <Xlcjs17@gmail.com>
2019-01-24 08:43:19 +0800

    整理_仓库本地化
    整理C代码；添加PHP学习分组。

---
+++ C/c_text_001.c
@@ -0,0 +1,11 @@
+//第一周
+/*scanf()、printf()、IF/switch/循环结构 
+*/ 
+#include<stdio.h>
+
+int main()
+{
+    int radius;
+    scanf("%d",&radius);
+    return 0;
+}
---
+++ PHP/Demo1/001howto$.php
@@ -0,0 +1,12 @@
+<?php
+$username='111';
+$lastNmae="222";
+echo &username&lastNmae;
+echo'</br>';
+
+//声明整形
+// //十进制
+// $var123='123';
+// echo "$var123";
+//
+ ?>
