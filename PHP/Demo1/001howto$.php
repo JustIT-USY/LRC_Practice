9fbcccba2381f09e01118e6de9d80ac50082b24d
Xlcjs <Xlcjs17@gmail.com>
2019-02-01 14:35:32 +0800

    注意变量符号为$

--- PHP/Demo1/001howto$.php
+++ PHP/Demo1/001howto$.php
@@ -1,8 +1,15 @@
 <?php
 $username='111';
 $lastNmae="222";
-echo &username;
-
+echo $username;
 echo'</br>';
-echo'$username'//' 会输出‘’其中的内容；
+echo $lastNmae;
+echo'</br>';
+echo '习惯使用小骆驼 （骆驼标记法） 第一单词首字母要小写，后面单词字母大写';
+echo '例如：$firstNameLastName...';
+$firstNameLastName='333';
+echo'</br>';
+echo $firstNameLastName;
+echo'</br>';
+echo'$username'//' 会输出''其中的内容；
  ?>
