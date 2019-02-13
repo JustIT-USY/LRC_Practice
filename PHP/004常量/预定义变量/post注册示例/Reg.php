b3ec98ad563ce4fb69e5df55aad5deb71c597749
Xlcjs <Xlcjs17@gmail.com>
2019-02-13 19:07:03 +0800

    post注册示例

---
+++ PHP/004常量/预定义变量/post注册示例/Reg.php
@@ -0,0 +1,11 @@
+<?php
+//接受表单发送数据
+//利用$_POST[名称] 接受数据
+
+echo '用户名：',$_POST['username'],'<br/>';
+echo '密码：',$_POST['password'],'<br/>';
+echo '邮箱：',$_POST['email'],'<br/>';
+echo '性别：',$_POST['sex'],'<br/>';
+
+
+ ?>
