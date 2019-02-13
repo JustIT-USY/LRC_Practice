26acd1194e9190b6c9926cc6ca45727f8b27dc97
Xlcjs <Xlcjs17@gmail.com>
2019-02-13 21:05:23 +0800

    \$_REQUSET 存在问题

--- PHP/004常量/预定义变量/get示例/link.php
+++ PHP/004常量/预定义变量/get示例/link.php
@@ -1,9 +1,16 @@
 <?php
 //接受超链接发送过来的数据
 // print_r($_GET);
+/*
 echo $_GET['username'],'<br/>';
 echo $_GET['age'],'<br/>';
 echo $_GET['a'],'<br/>';
 echo $_GET['b'],'<br/>';
 echo $_GET['c'],'<br/>';
+*/
+
+//  ini_set("error_reporting","E_ALL & ~E_NOTICE"); // 解决不要显示php通知的语句
+// $_REQUSET变量
+echo $_REQUSET['username'];//会显示Notice: Undefined variable: _REQUSET
+
  ?>
