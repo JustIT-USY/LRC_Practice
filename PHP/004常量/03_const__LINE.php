29e6e56539c97e1b775d8fb8e0b2d3a823c86045
Xlcjs <Xlcjs17@gmail.com>
2019-02-13 11:48:53 +0800

    魔术常量

---
+++ PHP/004常量/03_const__LINE.php
@@ -0,0 +1,19 @@
+<?php
+//魔术常量
+//两个下划线开始中间大写字母两个下划线结束
+/*其他魔术常量
+__FUNCTON__
+__CLASS__
+__METHOD__
+__TRAIT__
+__NAMESPACE__
+*/
+echo __LINE__;//10
+echo '<br/>';
+echo __FILE__;//绝对路径
+echo '<br/>';
+echo __DIR__;//文件夹路径
+
+
+
+ ?>
