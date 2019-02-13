2f487959e210fa83c955a1e879102ec695f97496
Xlcjs <Xlcjs17@gmail.com>
2019-02-13 21:06:29 +0800

    使用$_POST 和 $_REQUEST 两种方法传递数据

--- PHP/004常量/预定义变量/post注册示例/Reg.php
+++ PHP/004常量/预定义变量/post注册示例/Reg.php
@@ -1,11 +1,15 @@
 <?php
 //接受表单发送数据
 //利用$_POST[名称] 接受数据
-
+/*
 echo '用户名：',$_POST['username'],'<br/>';
 echo '密码：',$_POST['password'],'<br/>';
 echo '邮箱：',$_POST['email'],'<br/>';
 echo '性别：',$_POST['sex'],'<br/>';
-
-
+*/
+//测试request，指定了使用post方式可以成功！
+echo $_REQUEST['username'],'<br/>';
+echo $_REQUEST['password'],'<br/>';
+echo $_REQUEST['email'],'<br/>';
+echo $_REQUEST['sex'],'<br/>';
  ?>
