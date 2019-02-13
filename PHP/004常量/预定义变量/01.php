fadc4347e89ddb83c80ccc365fa8203e8a8092d6
Xlcjs <Xlcjs17@gmail.com>
2019-02-13 19:08:18 +0800

    预定义变量

---
+++ PHP/004常量/预定义变量/01.php
@@ -0,0 +1,26 @@
+<?php
+//PHP中预定义变量->由PHP提供的直接可使用的全局变量
+/*
+$globals  ->  超全局变量，包含以下所有的预定义变量
+$_SERVERS ->服务器和执行环境信息变量
+$_ENV ->环境变量
+$_COOKIE ->HTTP Cookies
+$_SESSION ->HTTP Session变量
+$_FILES ->文件上传信息变量
+$_GET ->HTTP GET变量
+$_POST ->HTTP POST变量
+$_REQUEST ->包含$_GET;$_POST;$_COOKIE
+
+*/
+/*
+接受表单发送数据
+利用$_POST[名称] 接受数据
+*/
+/*
+//HTTP GET变量
+//主要接受以？形式传递的数据
+//像表单以get形式发送数据，包括像超链接
+//形式：$_GET[名称]
+*/
+
+ ?>
